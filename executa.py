ec3c8082fd85011aa001093c74718427a777e01d
Maqdo <gabriiel.macedo97@gmail.com>
2019-10-23 16:54:39 -0400

    identificaçao de tokens

---
+++ executa.py
@@ -0,0 +1,4 @@
+from lexico import AnalisadorLexico
+
+A_lexico = AnalisadorLexico()
+A_lexico.analisador()
--- lexico.py
+++ lexico.py
@@ -1,6 +1,6 @@
 #biblioteca para entrada e saida de arquivos
 import sys
-import os.path
+#import os.path
 #biblioteca para string.*
 import string
 
@@ -24,6 +24,12 @@ class AnalisadorLexico():
         if entrada in caractere:
             return True
         return False
+        
+    def verEspace(self, entrada):
+        espaco = string.whitespace
+        if entrada in espaco:
+            return True
+        return False
 
     #Verifica se a entrada eh letra
     def verLetras(self, entrada):
@@ -41,17 +47,38 @@ class AnalisadorLexico():
 
     #Verifica se a entrada eh operador
     def verOperadores(self, entrada):
-        operadores = '. + - * / == != > >= < <= = :='.split()
+        operadores = '. + - * / == != > >= < <= = := :'.split()
         if entrada in operadores:
             return True
         return False
 
+    #Verifica qual token operador
+    def tokenOperador(self, entrada):
+        operadores = ". + - * / == != > >= < <= = := :".split()
+        i = operadores.index(entrada) #posicao
+        return "token "+str(i)
+        
+        #for x in operadores:
+        #    if x == entrada:
+        #        break
+        #    i += 1
+        #if i > 9:
+        #    return "token"+str(i)
+        #else:
+        #    return "token"+str(i)
+
     #Verifica se a entrada eh delimitador
     def verDelimitador(self, entrada):
-        paradores = '; , ( ) [ ] { }'.split()
+        paradores = "; , ( ) [ ] { }".split()
         if entrada in paradores:
             return True
         return False
+
+    #Verifica token delimitador
+    def tokenDelimitador(self, entrada):
+        paradores = "; , ( ) [ ] { }".split()
+        posicao = paradores.index(entrada)
+        return "token "+str(posicao)
 
     #Verifica se a entrada eh palavra reservada
     def verReservada(self, entrada):
@@ -59,6 +86,12 @@ class AnalisadorLexico():
         if entrada in palavra:
             return True
         return False
+
+    #Verifica Token reservada
+    def tokenReservada(self, entrada):
+        palavra = "program begin end var real integer procedure else read write while if do then".split()
+        i = palavra.index(entrada)
+        return "token "+str(i)
 
     #Execucao do analisador
     def analisador(self):
@@ -84,16 +117,16 @@ class AnalisadorLexico():
                     prox_char = linha_codigo[i+1]
                 #Delimitador
                 if self.verDelimitador(char_atual):
-                    arquivo_saida.write(char_atual+"\n")
+                    arquivo_saida.write(self.tokenDelimitador(char_atual)+' '+char_atual+'->'+str(nLinha)+'\n')
                 #Comentarios
-                elif char_atual == "#":
+                elif char_atual == "/" and prox_char == "/":
                     i = tam_linha
                 #Operador
                 elif prox_char != None and self.verOperadores(char_atual+prox_char):
-                    arquivo_saida.write(self.char_atual+prox_char+"\n")
+                    arquivo_saida.write(self.tokenOperador(char_atual+prox_char)+' '+char_atual+prox_char+'->'+str(nLinha)+'\n')
                     i += 1
                 elif self.verOperadores(char_atual):
-                    arquivo_saida.write(self.char_atual+"\n")
+                    arquivo_saida.write(self.tokenOperador(char_atual)+' '+char_atual+'->'+str(nLinha)+'\n')
                 #Numero
                 elif self.verNumeros(char_atual):
                     aux = char_atual
@@ -125,17 +158,17 @@ class AnalisadorLexico():
                             while i+1 < tam_linha:
                                 i += 1
                                 char_atual = linha_codigo[i]
-                                if self.verDelimitador(char_atual) or char_atual == ' ':
+                                if self.verDelimitador(char_atual) or self.verEspace(char_atual): #char_atual == ' ':
                                     i -= 1 #Volta um caractere da linha para que o delimitador seja reconhecido no momento certo
                                     break
                         else:
                             arquivo_saida.write('Erro Lexico - Numero mal formado - Linha: %d\n' %nLinha)
                         if (j > 0):
-                            arquivo_saida.write(aux+'->'+str(nLinha)+'\n')
+                            arquivo_saida.write('token '+aux+'->'+str(nLinha)+'\n')
                         else:
                             arquivo_saida.write('Erro Lexico - Numero mal formado - Linha: %d\n' %nLinha)
                     else:
-                        arquivo_saida.write(aux+'->'+str(nLinha)+'\n')
+                        arquivo_saida.write('token '+aux+'->'+str(nLinha)+'\n')
                         if(not self.verNumeros(char_atual)):
                             i -= 1
 				#Palavras reservadas
@@ -151,10 +184,10 @@ class AnalisadorLexico():
                             prox_char = linha_codigo[i+1]
                         if self.verLetras(char_atual) or self.verNumeros(char_atual) or char_atual == '_':
                             aux += char_atual
-                        elif self.verDelimitador(char_atual) or char_atual == ' ' or char_atual == '\t' or char_atual == '\r':
+                        elif self.verDelimitador(char_atual) or self.verEspace(char_atual): #char_atual == ' ' or char_atual == '\t' or char_atual == '\r':
                             i -= 1 #Volta um caractere da linha para que o delimitador seja reconhecido no momento certo
                             break
-                        elif prox_char != None and self.verOperadores(char_atual+prox_char) or self.verOperadores(char_atual):
+                        elif prox_char != None and self.verOperadores(char_atual+prox_char) or self.verOperadores(char_atual) or char_atual == ':':
                             i -= 1
                             break
                         elif char_atual != '\n':
@@ -168,19 +201,23 @@ class AnalisadorLexico():
                         while (i+1 < tam_linha):
                             i += 1
                             char_atual = linha_codigo[i]
-                            if self.verDelimitador(char_atual) or char_atual == ' ' or char_atual == '\t' or char_atual == '\r' or char_atual == '/':
+                            if self.verDelimitador(char_atual) or self.verEspace(char_atual): #char_atual == ' ' or char_atual == '\t' or char_atual == '\r' or char_atual == '/':
 								i -= 1 #Volta um caractere da linha para que o delimitador seja reconhecido no momento certo
 								break
                     else: #Se nao houver erros basta verificar se o elemento eh uma palavra reservada
                         if (self.verReservada(aux)):
-                            arquivo_saida.write(aux+'->'+str(nLinha)+'\n')
+                            arquivo_saida.write(self.tokenReservada(aux)+" "+aux+'->'+str(nLinha)+'\n')
                         else:
-                            arquivo_saida.write(aux+'->'+str(nLinha)+'\n') #Identificador
+                            arquivo_saida.write('token '+aux+'->'+str(nLinha)+'\n') #Identificador
 				#Caractere invalido
-                elif char_atual != '\n' and char_atual != ' ' and char_atual != '\t' and char_atual != '\r':
+                elif not self.verEspace(char_atual): #char_atual != '\n' and char_atual != ' ' and char_atual != '\t' and char_atual != '\r':
                     arquivo_saida.write('Erro Lexico - Caracter Invalido: ' + char_atual + ' - linha: %d\n' %nLinha)
-				#Indo para o proximo caractere da linha
+                
+                elif self.verEspace(char_atual):
+                    arquivo_saida.write('--------------------\n')
+                #Indo para o proximo caractere da linha
                 i += 1
+            
             linha_codigo = arquivo_entrada.readline() # Le a proxima linha
             nLinha += 1
 		#Fim do programa
